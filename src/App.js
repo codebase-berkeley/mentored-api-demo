bbbe3630c0472890fcf7efc7ff9db08ea9cb701f
Kennedy <kennedyhchung@gmail.com>
2023-02-28 22:38:47 -0800

    Template for the dog genreator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import doggo1 from './assets/dog1.gif';
+import doggo2 from './assets/dog2.gif';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  function generateDog() {
+    alert("Hehehe generate a dog!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Title of the dog generator */}
+      <div className="title">🐕 Doggo Generator 🐕</div>
+      {/* Body that contains the dog image */}
+      <div className="body">
+        <div className="sideColumn">
+			<img src={doggo1} alt="dog1"/>
+        </div>
+        <div class="centerColumn">
+          <img className="dog" alt=""/>
+          <button className="button1" onClick={generateDog}>Generate</button>;
+        </div>
+        <div className="sideColumn">
+			<img src={doggo2} alt="dog2"/>
+		</div>
+      </div>
     </div>
   );
 }
